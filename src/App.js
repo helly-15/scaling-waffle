17c404ec4e3fdbe18c03351f49efe9c875fba6d1
Lenocka <lvutina.en@gmail.com>
2021-01-22 20:38:24 +0300

    feat:add paws grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.scss';
 import './variablesSCSS/_variables.scss';
 import logo from './assets/StudioLogo.png'
-import PawRow from "./navigation/PawRow";
+import Paw from "./navigation/Paw";
+import React from "react";
 function App() {
   return (
       <div className='overlay'>
@@ -12,14 +13,24 @@ function App() {
                   <source srcSet={logo}/>
                   <img className='header__logo_img' src={logo} alt="company Logo"/>
               </picture>
+              <ul className='navigation'>
+                  <Paw className ={'first'} text ={'First Paw'}/>
+                  <Paw className ={'second'}/>
+                  <Paw className ={'third'}/>
+                  <Paw className ={'fourth'}/>
+                  <Paw className ={'fifth'}/>
+
+              </ul>
           </header>
           <main>
-              <section className='navigation'>
-                  <PawRow/>
-                  <PawRow/>
-                  <PawRow/>
-                  <PawRow/>
-              </section>
+              {/*<svg viewBox="0 0 425 300">*/}
+              {/*    <path id="curve" d="M6,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145" />*/}
+              {/*    <text x="25">*/}
+              {/*        <textPath href="#curve">*/}
+              {/*            Dangerous Curves Ahead*/}
+              {/*        </textPath>*/}
+              {/*    </text>*/}
+              {/*</svg>*/}
           </main>
           <footer>
           </footer>
--- src/navigation/Paw.jsx
+++ src/navigation/Paw.jsx
@@ -1,9 +1,37 @@
-import './Paw.scss'
-function Paw(){
+import './Paw.scss';
+import paw from '../assets/CatPaw.png'
+import React from "react";
+function Paw(props){
+    const {className, text} =props;
     return(
-        <div className='navigation-paw'>
-        </div>
+
+        <li className={ `${className} navigation-paw`}>
+            {/*<svg  className={ `${className} navigation-paw__description`}>*/}
+            {/*    <path id="curve" d="M6,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145" />*/}
+            {/*    <text x="25">*/}
+            {/*        <textPath href="#curve">*/}
+            {/*            Dangerous Curves Ahead*/}
+            {/*        </textPath>*/}
+            {/*    </text>*/}
+            {/*</svg>*/}
+
+            {/*<svg viewBox="0 0 500 500">*/}
+            {/*    <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />*/}
+            {/*    <text width="500">*/}
+            {/*        <textPath href="#curve">*/}
+            {/*            Dangerous Curves Ahead*/}
+            {/*        </textPath>*/}
+            {/*    </text>*/}
+            {/*</svg>*/}
+            <div>
+                <img className={ `${className} navigation-paw__img`} src={paw}/>
+            </div>
+
+        </li>
     )
 }
 
 export default Paw;
+
+
+
