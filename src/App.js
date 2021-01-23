a85e279c5b8f9e342cbc0ce0630e9042d29d42d5
Lenocka <lvutina.en@gmail.com>
2021-01-23 16:15:12 +0300

    fix:add text to paws, remove svg text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import logo from './assets/StudioLogo.png'
 import Paw from "./navigation/Paw";
 import React from "react";
 function App() {
+    const MAIN_NAV_ITEMS=['Catository', 'Projects', 'Cat Master', 'Catstogram','testqqqqqqq'];
+    const MAIN_NAV_CLASSNAMES =['first', 'second', 'third', 'fourth', 'fifth']
+    const navigation =[];
+    for (let i = 0;i < MAIN_NAV_ITEMS.length; i++) {
+        navigation.push(<Paw className={ MAIN_NAV_CLASSNAMES[i] } text={ MAIN_NAV_ITEMS[i]} key = {`navButton${i}` }/>)
+    }
   return (
       <div className='overlay'>
         <div className="app">
@@ -14,23 +20,18 @@ function App() {
                   <img className='header__logo_img' src={logo} alt="company Logo"/>
               </picture>
               <ul className='navigation'>
-                  <Paw className ={'first'} text ={'First Paw'}/>
-                  <Paw className ={'second'}/>
-                  <Paw className ={'third'}/>
-                  <Paw className ={'fourth'}/>
-                  <Paw className ={'fifth'}/>
-
+                  {navigation}
               </ul>
           </header>
           <main>
-              {/*<svg viewBox="0 0 425 300">*/}
-              {/*    <path id="curve" d="M6,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145" />*/}
-              {/*    <text x="25">*/}
-              {/*        <textPath href="#curve">*/}
-              {/*            Dangerous Curves Ahead*/}
-              {/*        </textPath>*/}
-              {/*    </text>*/}
-              {/*</svg>*/}
+              <svg viewBox="0 0 425 300">
+                  <path id="curve" d="M-20,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145" />
+                  <text x="50">
+                      <textPath href="#curve">
+                          Catository
+                      </textPath>
+                  </text>
+              </svg>
           </main>
           <footer>
           </footer>
--- src/navigation/Paw.jsx
+++ src/navigation/Paw.jsx
@@ -6,26 +6,11 @@ function Paw(props){
     return(
 
         <li className={ `${className} navigation-paw`}>
-            {/*<svg  className={ `${className} navigation-paw__description`}>*/}
-            {/*    <path id="curve" d="M6,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145" />*/}
-            {/*    <text x="25">*/}
-            {/*        <textPath href="#curve">*/}
-            {/*            Dangerous Curves Ahead*/}
-            {/*        </textPath>*/}
-            {/*    </text>*/}
-            {/*</svg>*/}
+                    <img className={ 'navigation-paw__img'} src={paw}/>
 
-            <svg className={ 'navigation-paw__text'} viewBox="50 0 200 200">
-                <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-                <text >
-                    <textPath href="#curve">
-                        Dangerous Curves Ahead
-                    </textPath>
-                </text>
-            </svg>
-            <div>
-                <img className={ 'navigation-paw__img'} src={paw}/>
-            </div>
+            <span className={'navigation-paw__text'}>
+                {text}
+            </span>
 
         </li>
     )
