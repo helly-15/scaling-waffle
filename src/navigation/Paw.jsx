acc281820abbd7457f85793fbcf78fd4d1bf87cb
Lenocka <lvutina.en@gmail.com>
2021-01-24 23:06:10 +0300

    feat:add animation to paws

--- src/navigation/Paw.jsx
+++ src/navigation/Paw.jsx
@@ -5,7 +5,7 @@ function Paw(props){
     const {className, text} =props;
     return(
 
-        <li className={ `${className} navigation-paw`}>
+        <li className={ `${className} navigation-paw`} onAnimationEnd={(e)=>{e.target.style.opacity = '1'}}>
                     <img className={ 'navigation-paw__img'} src={paw}/>
 
             <span className={'navigation-paw__text'}>
