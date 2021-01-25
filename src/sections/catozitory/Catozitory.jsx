8dd72414b591f2637ed58e01deb2f44958e09b48
Lenocka <lvutina.en@gmail.com>
2021-01-25 20:05:36 +0300

    feat:add names to projects

--- src/sections/catozitory/Catozitory.jsx
+++ src/sections/catozitory/Catozitory.jsx
@@ -5,13 +5,18 @@ function Catozitory(props){
     //const {title, className, viewBox, x} =props;
     return(
         <section className='catository'>
-            <SectionTitle title = {'Catository'} viewBox ={"0 0 1440 320"} x ={"550"}/>
+            <SectionTitle title = {'Projects'} viewBox ={"0 0 1440 320"} x ={"600"}/>
             <div className='catository-tank'>
-                <div className='catository-tank__fish_one'>
 
+                <div className='catository-tank__fish_one'>
+                    <a href ='https://datagrid-virtualization-localstorage.netlify.app/' className='projects-text'>
+                        React+Redux
+                    </a>
                 </div>
                 <div className='catository-tank__fish_two'>
-
+                    <a href ='https://musing-cray-b5e1fc.netlify.app' className='projects-text'>
+                        Group project+Git
+                    </a>
                 </div>
                 <div className='catository-tank__fish_three'>
 
